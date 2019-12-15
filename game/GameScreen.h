a57d1f8d6e259c34ad85df37341c1057d0ed8846
mstefanowski <maciej.stefanowski6@gmail.com>
2019-12-15 20:24:44 +0100

    przerzucenie rysowania gry z maina do klasy GameScreen

--- game/GameScreen.h
+++ game/GameScreen.h
@@ -1,10 +1,13 @@
 #ifndef GAMESCREEN_H
 #define GAMESCREEN_H
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
-class GameScreen {
-public:
-    GameScreen();
-    void start();
+
+class GameScreen{
+	public:
+		GameScreen();
+		void start(RenderWindow &app);
 };
 
 
